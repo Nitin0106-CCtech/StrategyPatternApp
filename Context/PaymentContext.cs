457902ebdec6d8594f754b8cd1cefb893c59aa0e
Nitin0106-CCtech <nitin.kumar@o365.cctech.co.in>
2026-06-03 12:04:47 +0530

    Implement PaymentContext with strategy pattern

---
+++ Context/PaymentContext.cs
@@ -0,0 +1,24 @@
+using StrategyPatternApp.Strategies;
+
+namespace StrategyPatternApp.Context
+{
+    public class PaymentContext
+    {
+        private IPaymentStrategy _paymentStrategy;
+
+        public PaymentContext(IPaymentStrategy paymentStrategy)
+        {
+            _paymentStrategy = paymentStrategy;
+        }
+
+        public void SetPaymentStrategy(IPaymentStrategy paymentStrategy)
+        {
+            _paymentStrategy = paymentStrategy;
+        }
+
+        public void ProcessPayment(double amount)
+        {
+            _paymentStrategy.Pay(amount);
+        }
+    }
+}
