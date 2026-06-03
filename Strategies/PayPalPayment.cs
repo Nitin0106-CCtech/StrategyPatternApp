3f903038669c5c78f244a9a64770ef31bfc8848b
Nitin0106-CCtech <nitin.kumar@o365.cctech.co.in>
2026-06-03 12:08:41 +0530

    Implement PayPal payment strategy

---
+++ Strategies/PayPalPayment.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace StrategyPatternApp.Strategies
+{
+    public class PayPalPayment : IPaymentStrategy
+    {
+        public void Pay(double amount)
+        {
+            Console.WriteLine($"Paid ₹{amount} using PayPal.");
+        }
+    }
+}
